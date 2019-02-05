e47b5157f88289d1b6926b897ab1defc6b9775d4
Boris Strahilov <borisstrahilov@gmail.com>
2019-02-04 18:17:13 -0600

    updated db login credentials
    Now uses env variables rather than hardcoded values that are visible to the public.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: `mongodb://bstrahilov:<dbpassword>@ds221155.mlab.com:21155/garagebuddy-prod`
+    mongoURI: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds221155.mlab.com:21155/garagebuddy-prod`
   }
 } else {
   module.exports = {
